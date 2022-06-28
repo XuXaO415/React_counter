210a85138b19e6819b2955bd32bddd8479be6a4f
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-06-27 18:17:46 -0700

    Added class component

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
 class Counter extends React.Component {
+  constructor(props) {
+    //super is a reference to the parent class
+    super(props);
+    this.state = { count: 0, isHiding: false};
+  }
   render() {
+   const {color} = this.props;
+   const {count} = this.state;
     return (
       <div>
-        <h1>I am counter</h1>
+        <h1 style={{ color }}>I am counter</h1>
+        <h3>Count is: {this.state.count}</h3>
+        <button onClick={() => this.setState({count: count + 1})}>Add 1</button>
       </div>
     );
   }
 }
+
+export default Counter;
