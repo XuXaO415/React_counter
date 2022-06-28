609cc51b4346e886a459baee27f3ffa0440381da
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-06-27 18:21:20 -0700

    added setState in a class componenet

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -14,6 +14,7 @@ class Counter extends React.Component {
         <h1 style={{ color }}>I am counter</h1>
         <h3>Count is: {this.state.count}</h3>
         <button onClick={() => this.setState({count: count + 1})}>Add 1</button>
+        <button onClick={() => this.setState({count: count - 1})}>Subtract</button>
       </div>
     );
   }
