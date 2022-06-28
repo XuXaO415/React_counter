8e1b368b873ad6c16c2d50e93e828ed1d8fab75f
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-06-27 18:52:51 -0700

    Added bind

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -5,6 +5,18 @@ class Counter extends React.Component {
     //super is a reference to the parent class
     super(props);
     this.state = { count: 0, isHiding: false};
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+  increment = () => {
+    console.log(this.state.count);
+    console.log(this);
+    this.setState({ count: this.state.count + 1 });
+  }
+  decrement = () => {
+    console.log(this.state.count);
+    console.log(this);
+    this.setState({ count: this.state.count - 1 });
   }
   render() {
    const {color} = this.props;
@@ -12,9 +24,11 @@ class Counter extends React.Component {
     return (
       <div>
         <h1 style={{ color }}>I am counter</h1>
-        <h3>Count is: {this.state.count}</h3>
-        <button onClick={() => this.setState({count: count + 1})}>Add 1</button>
-        <button onClick={() => this.setState({count: count - 1})}>Subtract</button>
+        <h3>Count is: {count}</h3>
+        {/* <button onClick={() => this.setState({count: count + 1})}>Add 1</button>
+        <button onClick={() => this.setState({count: count - 1})}>Subtract</button> */}
+        <button onClick={this.increment}>Add 1</button>
+        <button onClick={this.decrement}>Subtract</button>
       </div>
     );
   }
